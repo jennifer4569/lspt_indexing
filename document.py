e8ccc77eb9a59fe3677eb9b92c0b739d24ee2c26
earlst <tabitha.earls@gmail.com>
2021-12-08 11:23:58 -0500

    Refactored document class

--- document.py
+++ document.py
@@ -1,27 +1,27 @@
 class Document(object):
+    #tokens is a dictionary
 
     # constructor
     def __init__(self, name, numWords):
         self.docName = name
         self.numWords = numWords
-        self.score = 0
-        self.positionalInfo = {}
+        self.tokens = {}
 
-    def calculateDocScore(self, tokenInfo):
+    def calculateScore(self, tokenInfo):
         weights = {
             "title": 1.0, 
             "headers": 0.75, 
             "links": 0.5,
             "other": 0.25
         }
-        
+
         total = 0
         multiplier = 0
         for position, frequency in tokenInfo:
             # what weight each position has
             multiplier = weights.get(position, 0)
             total += multiplier * frequency
-        
+
         # divide the total score by the number of words in the document so as to not favor larger documents    
         total /= self.numWords
             
@@ -29,18 +29,11 @@ class Document(object):
         if total == 0:
             raise Exception("Not enough information to calculate score")
         return total
+
+    def addToken(self, tokenName, positionInfo):
+        score = calculateScore(positionInfo)
+        self.tokens[tokenName] = (positionInfo, score)
     
-    def getName(self):
-        return self.docName
-    
-    def getScore(self):
-        return self.score
-    
-    def setScore(self, newScore):
-        self.score = newScore
-        
-    def setPositionalInfo(self, tokenInfo):
-        self.positionalInfo = tokenInfo
-        
-    def getPositionalInfo(self):
-        return self.positionalInfo
+    def getTokenInfo(self, tokenName):
+        return self.tokens[tokenName]
+
