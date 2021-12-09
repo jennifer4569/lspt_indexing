e1dfe0c04150141bfe6ddf40d35478179df3ab91
Jennifer Zhang <jennifer.newyork@aol.com>
2021-12-09 11:31:19 -0500

    updated comments based on new changes

--- indexing.py
+++ indexing.py
@@ -1,29 +1,31 @@
 import heapq
 from document import Document
 from doc_dict import DocDict
-# indexingData dictionary: key is the token, value is the priority queue of documents
+
+# indexingData dictionary: key is the token, value is the priority queue of document IDs
 indexingData = {}
+
+# docDict dictionary: key is the document ID, value is the respective Document
 docDict = DocDict()
 
-# addIndex(token, doc): adds the document to indexes, based on the token
+# addIndex(token, docID, score, indexes): adds the document to indexes, based on the token
 # 
 # inputs: token (string): the token to add the document in
-#         doc (Document): the document to add to indexes
+#         docID (string): the ID of the document to add to indexes
+#         score (double): score of the token within document-- to be used as priority in heap
 #         indexes (dictionary): the indexing data to add this document to -- defaults to indexingData
-#         score (integer): score of the token within document-- to be used as priority in heap
-#         positions (dictionary): gives frequencies for "title", "headers",  "links", and "other"
 # modifies: indexes
 # returns: none
-# notes: none
-def addIndex(token, docId, score, indexes = indexingData):
+# notes: adds token to indexes if it doesn't already exist
+def addIndex(token, docID, score, indexes = indexingData):
     # if the token doesn't exist, add the token in, initializing its priority queue
     if(not token in indexes):
         indexes[token] = []
 
     # adds the document into the respective token's priority queue
-    heapq.heappush(indexes[token], (score, docId))
+    heapq.heappush(indexes[token], (score, docID))
 
-# getTopNDocs(token, n): gets the top n documents associated with the given token
+# getTopNDocs(token, n, indexes): gets the top n documents associated with the given token
 #
 # inputs: token (string): the token to get the top n documents from
 #         n (integer): the number of top documents to retrieve from the respective token
@@ -39,11 +41,11 @@ def getTopNDocs(token, n = 1, indexes = indexingData):
     # loop through and return the top n documents associated with the token
     docs = []
     for doc in heapq.nlargest(n, indexes[token]):
-        docId = doc[1]
-        docs.append(docDict.getDocument(docId))
+        docID = doc[1]
+        docs.append(docDict.getDocument(docID))
     return docs
 
-# getAllDocs(token): gets all the documents associated with the given token
+# getAllDocs(token, indexes): gets all the documents associated with the given token
 #
 # inputs: token (string): the token to get all the documents from
 #         indexes (dictionary): the indexing data to get docs from -- defaults to indexingData
@@ -57,7 +59,7 @@ def getAllDocs(token, indexes = indexingData):
 
     return getTopNDocs(token, len(indexes[token]), indexes)
 
-# getAllTokens(): gets all the indices stored in indexes
+# getAllTokens(indexes): gets all the indices stored in indexes
 #
 # inputs: indexes (dictionary): the indexing data to get tokens from -- defaults to indexingData
 # modifies: none
@@ -69,36 +71,42 @@ def getAllTokens(indexes = indexingData):
         tokens.append(token)
     return tokens
 
-# updateDoc(token, doc, indexes): updates the document with its new document score
+# updateDoc(token, docID, score, indexes): updates the document with its new document score
 #
 # inputs: token (string): the token associated with the given document 
-#         docId (string): the document to update
+#         docID (string): the document to update
 #         score (double): the new score
 #         indexes (dictionary): the indexing data to apply this update to -- defaults to indexingData
 # modifies: indexes
 # returns: none
 # notes: adds token if the token doesn't exist
 #        adds document within the token if the document doesn't exist within the token
-def updateDoc(token, docId, score, indexes = indexingData):
+def updateDoc(token, docID, score, indexes = indexingData):
+    # adds token if the token doesn't already exist
     if(not token in indexes):
         indexes[token] = []
+
+    # searches for the document within the token's priority queue
     val = indexes.get(token)
     docExists = False
     for element in val:
-        if(element[1] == docId):
+        # found the document -- update score
+        if(element[1] == docID):
             indexes[token].remove(element)
-            heapq.heappush(indexes[token], (score, docId))
+            heapq.heappush(indexes[token], (score, docID))
             docExists = True
             break
-    if(not docExists):
-        heapq.heappush(indexes[token], (score, docId))
 
-# clearIndexes(): clears all the indexing data that has been stored
+    # adds document if the document doesn't exist in the token
+    if(not docExists):
+        heapq.heappush(indexes[token], (score, docID))
+
+# clearIndexes(): clears all the indexing data that has been stored, including docDict
 #
 # inputs: indexes (dictionary): the indexing data to clear -- defaults to indexingData
-# modifies: indexes
+# modifies: indexes, docDict
 # returns: none
-# notes: none
+# notes: clears both indexes and docDict
 def clearIndexes(indexes = indexingData):
     indexes.clear()
     docDict.clearDict()
