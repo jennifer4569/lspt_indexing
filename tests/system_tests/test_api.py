911debf0ee21a3107baa78b11220f906361ad226
Jennifer Zhang <jennifer.newyork@aol.com>
2021-12-10 11:07:09 -0500

    updated reasons for skipping api tests

--- tests/system_tests/test_api.py
+++ tests/system_tests/test_api.py
@@ -6,19 +6,19 @@ import pytest
 import test_indexing
 
 # test retrieving document metadata from text transformation
-@pytest.mark.skip(reason="text transformation not available yet")
+@pytest.mark.skip(reason="Text Transformation not ready to send information to us yet")
 def test23():
     # initial setup
     indexing.clearIndexes()
 
 # test sending indexing data to ranking
-@pytest.mark.skip(reason="not ready to send to ranking yet")
+@pytest.mark.skip(reason="Ranking not ready to receive information from us yet")
 def test24():
     # initial setup
     indexing.test10()
 
 # test an indexing query that contains multiple tokens
-@pytest.mark.skip(reason="api not ready yet")
+@pytest.mark.skip(reason="Ranking not ready to receive information from us yet")
 def test28():
     # initial setup
     indexing.test10()
